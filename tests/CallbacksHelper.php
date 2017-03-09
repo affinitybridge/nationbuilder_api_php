f476c1a03f7f957b007d546364c68c518ed37c98
Raphael Huefner <raphael@affinitybridge.com>
2017-03-09 03:57:39 -0800

    more debugging info for tests

--- tests/CallbacksHelper.php
+++ tests/CallbacksHelper.php
@@ -8,6 +8,7 @@ use Affinitybridge\NationBuilder\Api as Api;
 
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Psr7\Request as GuzzleRequest;
+use GuzzleHttp\Exception\ClientException as ClientException;
 
 class CallbacksHelper {
     protected $credentials = [];
@@ -22,7 +23,33 @@ class CallbacksHelper {
     public function httpCall($request, $makeResponseCallback) {
         $guzzleClient = new GuzzleClient();
         $guzzleRequest = new GuzzleRequest($request->getMethod(), (string) $request->getUri(), $request->getHeaders(), $request->getBody());
-        $guzzleResponse = $guzzleClient->send($guzzleRequest);
+        try {
+            $guzzleResponse = $guzzleClient->send($guzzleRequest);
+        }
+        catch (ClientException $e) {
+            // @todo print this only if the --verbose flag is set.
+            $response = $e->getResponse();
+            print PHP_EOL . PHP_EOL . "CAUGHT ClientException" . PHP_EOL;
+            print PHP_EOL . PHP_EOL . "REQUEST METHOD:" . PHP_EOL . PHP_EOL;
+            print $request->getMethod();
+            print PHP_EOL . PHP_EOL . "REQUEST URL:" . PHP_EOL . PHP_EOL;
+            print (string) $request->getUri();
+            print PHP_EOL . PHP_EOL . "REQUEST HEADERS:" . PHP_EOL . PHP_EOL;
+            print var_export($request->getHeaders(), true);
+            print PHP_EOL . PHP_EOL . "REQUEST BODY:" . PHP_EOL . PHP_EOL;
+            print $request->getBody();
+            print PHP_EOL . PHP_EOL . "STATUS CODE:" . PHP_EOL . PHP_EOL;
+            print $response->getStatusCode();
+            print PHP_EOL . PHP_EOL . "REASON PHRASE:" . PHP_EOL . PHP_EOL;
+            print $response->getReasonPhrase();
+            print PHP_EOL . PHP_EOL . "PROTOCOL VERSION:" . PHP_EOL . PHP_EOL;
+            print $response->getProtocolVersion();
+            print PHP_EOL . PHP_EOL . "RESPONSE HEADERS:" . PHP_EOL . PHP_EOL;
+            print var_export($response->getHeaders(), true);
+            print PHP_EOL . PHP_EOL . "RESPONSE BODY:" . PHP_EOL . PHP_EOL;
+            print $response->getBody();
+            throw $e;
+        }
         return $makeResponseCallback(
             $guzzleResponse->getStatusCode(),
             $guzzleResponse->getReasonPhrase(),
